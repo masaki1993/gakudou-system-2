de95b4fd3561d5a08066967236828a1fb7800fe0
masaki1993 <masakiyujiro@sene-g.com>
2024-06-26 16:28:07 +0900

    Update build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
-      'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
-    },
-    build: {
-      outDir: "./build"
-    },
-    server: {
-      host: '0.0.0.0'
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
